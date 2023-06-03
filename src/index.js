6a79986b6ed23d781cfb1b17060f3eb4c17989a5
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-03 19:31:37 +0300

    Remove PersistGate

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
-import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize';
 import { App } from 'components/App';
 import { theme } from './constants/theme';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store'; // persistor,
 import { GlobalStyle } from './GlobalStyle.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-          <GlobalStyle />
-        </PersistGate>
+        <App />
+        <GlobalStyle />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>

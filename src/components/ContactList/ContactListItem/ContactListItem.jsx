96aa499137fe338070548c5e749240c9e7641bbf
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-03 19:56:57 +0300

    Update props

--- src/components/ContactList/ContactListItem/ContactListItem.jsx
+++ src/components/ContactList/ContactListItem/ContactListItem.jsx
@@ -4,18 +4,20 @@ import {
   Contact,
   ContactItem,
   ContactName,
+  ContactNumber,
   DeleteButton,
 } from './ContactListItem.styled';
-import { deleteContact } from 'redux/contacts/contactsSlice';
+import { deleteContact } from 'redux/contacts/operations';
 
-export const ContactListItem = ({ id, name, number }) => {
+export const ContactListItem = ({ id, name, phone }) => {
   const dispatch = useDispatch();
   const onDelete = () => dispatch(deleteContact(id));
 
   return (
     <ContactItem>
       <Contact>
-        <ContactName>{name}:</ContactName> {number}
+        <ContactName>{name}: </ContactName>
+        <ContactNumber> {phone}</ContactNumber>
       </Contact>
       <DeleteButton type="button" onClick={onDelete}>
         Delete
@@ -27,5 +29,5 @@ export const ContactListItem = ({ id, name, number }) => {
 ContactListItem.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
 };
